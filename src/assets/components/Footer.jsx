cb98e7ac868c6eb5777c3b7e18b7414df1ca25e0
Manasvi-247 <manasvi.sabbarwal@gmail.com>
2025-05-25 01:23:52 +0530

    Footer component  added

---
+++ src/assets/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="mt-12 py-6 text-center text-sm text-gray-500 dark:text-gray-400">
+      © 2025 Finology. All rights reserved.
+      Made by love 🩷 Team Byte Me! 🩷
+    </footer>
+  );
+}
